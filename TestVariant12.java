92222cb4869780a6e93b0189cde0a210d500ce59
Bilousov-V <80888808+Bilousov-V@users.noreply.github.com>
2021-09-11 19:49:50 +0300

    Update TestVariant12.java

--- TestVariant12.java
+++ TestVariant12.java
@@ -20,7 +20,7 @@ public class TestVariant12 {
 
     @DataProvider
     public Object[][] integerProvider() {
-        return new Object[][] { { 110, 11 }, { 374, 473 }, {808, 808} };
+        return new Object[][]{{110, 11}, {374, 473}, {808, 808}};
     }
 
     @Test(expectedExceptions = AssertionError.class)
@@ -37,7 +37,7 @@ public class TestVariant12 {
 
     @DataProvider
     public Object[][] ifProvider() {
-        return new Object[][] { { 1, 2, 3, 1 }, { 25, 25, 25, 25 }, { -23, -69, -10, -69 } };
+        return new Object[][]{{1, 2, 3, 1}, {25, 25, 25, 25}, {-23, -69, -10, -69}};
     }
 
     //////////////////////////////////////////////////
@@ -49,27 +49,27 @@ public class TestVariant12 {
 
     @DataProvider
     public Object[][] booleanProvider() {
-        return new Object[][] { { 1, 1, 1, true }, { 0, 17, 42, false }, { -3, 57000, 69, false } };
+        return new Object[][]{{1, 1, 1, true}, {0, 17, 42, false}, {-3, 57000, 69, false}};
     }
 
     //////////////////////////////////////////////////
 
     @Test(dataProvider = "switchProvider")
-    public void switchTest(int p1, int p2, double[] arr) {
+    public void switchTest(int p1, double p2, double[] arr) {
         assertEquals(new Variant12().switchTask(p1, p2), arr);
     }
 
     @DataProvider
     public Object[][] switchProvider() {
-        double[] input1 = { 1, 2, 6.28, 3.14 };
-        double[] input2 = { 3, 6, 6.28 * 3, 3.14 * 3 * 3 };
-        double[] input3 = { 4, 8, 6.28 * 4, 3.14 * 4 * 4};
-        return new Object[][] { { 1, 1, input1 }, { 2, 6, input2 }, { 3, 6.28 * 4, input3 } };
+        return new Object[][]  {{1, 1, new double[] {1, 2, 6.28, 3.14}},
+                                {2, 6, new double[] {3, 6, 6.28 * 3, 3.14 * 3 * 3}},
+                                {3, 6.28 * 4, new double[] {4, 8, 6.28 * 4, 3.14 * 4 * 4}},
+                                {4, 3.14 * 10.1 * 10.1, new double[] {10.1, 20.2, 6.28 * 10.1, 3.14 * 10.1 * 10.1}}};
     }
 
     @Test(expectedExceptions = AssertionError.class)
     public void switchNegativeTest() {
-        new Variant12().forTask(10);
+        new Variant12().switchTask(9, -1);
     }
 
     ///////////////////////////////////////////////////
@@ -81,7 +81,12 @@ public class TestVariant12 {
 
     @DataProvider
     public Object[][] forProvider() {
-        return new Object[][] { { 3, 1.716 }, { 7, 9.801792 }, { 12, 309.744468 } };
+        return new Object[][]{{3, 1.716}, {7, 9.801792}, {12, 309.744468}};
+    }
+
+    @Test(expectedExceptions = AssertionError.class)
+    public void negativeForTest(){
+        new Variant12().forTask(-3);
     }
 
     ///////////////////////////////////////////////////
@@ -93,70 +98,80 @@ public class TestVariant12 {
 
     @DataProvider
     public Object[][] whileProvider() {
-        return new Object[][] { { 5, new int[] {3, 6} }, { 10, new int[] {4, 10} }, { 100, new int[] {14, 105} } };
+        return new Object[][]  {{5, new int[]{3, 6}},
+                                {10, new int[]{4, 10}},
+                                {100, new int[]{14, 105}}};
     }
 
-    @Test(expectedExceptions = AssertionError.class, dataProvider = "negativeWhileProvider")
-    public void negativeWhileTest(int n) {
-        new Variant12().whileTask(n);
-    }
-
-    @DataProvider
-    public Object[][] negativeWhileProvider() {
-        return new Object[][] { { 0 }, { -20 } };
+    @Test(expectedExceptions = AssertionError.class)
+    public void negativeWhileTest() {
+        new Variant12().whileTask(0);
     }
 
     ///////////////////////////////////////////////////
 
     @Test(dataProvider = "minMaxProvider")
-    public void minMaxTest(int[] arr, int n, int min){
-        assertEquals(new Variant12().minMaxTask(arr, n), min);
+    public void minMaxTest(int[] arr, int min) {
+        assertEquals(new Variant12().minMaxTask(arr), min);
     }
 
     @DataProvider
-    public Object[][] minMaxProvider(){
-        return new Object[][] { { new int[] {1, 2, 3, 4, 5, 0, 10}, 7, 1}, { new int[] { -1, -2 ,-69 }, 3, 0}, { new int[] { 0 }, 1, 0 } };
+    public Object[][] minMaxProvider() {
+        return new Object[][]  {{new int[]{1, 2, 3, 4, 5, 0, 10}, 1},
+                                {new int[]{-1, -2, -69}, 0},
+                                {new int[]{0}, 0}};
     }
 
     //////////////////////////////////////////
 
     @Test(dataProvider = "arrayProvider")
-    public void arrayTest(double[] array, double value) {
+    public void arrayTest(double[] array, double[] value) {
         assertEquals(new Variant12().arrayTask(array), value);
     }
 
     @DataProvider
     public Object[][] arrayProvider() {
-        return new Object[][] { { new double[] { 10, 2, 3 }, 0 }, { new double[] { 10, 2, 13 }, 10 },
-                { new double[] { 4, 3, 5, -4, 9, 2 }, -4 } };
+        return new Object[][]  {{new double[]{10.5, 11.6, 3.7, 21.21}, new double[] {11.6, 21.21}},
+                                {new double[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, new double[] {2, 4, 6, 8, 10}},
+                                {new double[]{-4, 3.14, 5, -4, 9.9999999, -2.71828}, new double[] {3.14, -4, -2.71828}}};
     }
 
-
+    @Test(expectedExceptions = AssertionError.class)
+    public void negativeArrayTest(){
+        new Variant12().arrayTask(new double[] {0, 1, 2});
+    }
 
     //////////////////////////////////////////
 
     @Test(dataProvider = "matrixProvider")
-    public void twoDimensionArrayTest(int[][] input, int from, int to, int[][] output) {
-        assertEquals(new Variant12().twoDimensionArrayTask(input, from, to), output);
+    public void matrixTest(int[][] input, int[][] output) {
+        assertEquals(new Variant12().matrixTask(input), output);
     }
 
     @DataProvider
     public Object[][] matrixProvider() {
-        int[][] input = {{2, 3, 6, 9, -9},
-                {34, 98, -9, 2, 1},
-                {-4, 2, 1, 6, 1},
-                {-98, 8, 1, 5, 3}};
+        int[][] input1 = {{1, 2},
+                          {3, 4}};
 
-        int[][] input23 = {{2, 3, 6, 9, -9},
-                {-4, 2, 1, 6, 1},
-                {34, 98, -9, 2, 1},
-                {-98, 8, 1, 5, 3}};
+        int[][] input2 = {{0, -10, -9, 1, 2, -8},
+                          {6,   6, -6, 6, 6, -6},
+                          {-9, 98,  9, 5, 5,  4}};
 
-        int[][] input14 = {{-98, 8, 1, 5, 3},
-                {-4, 2, 1, 6, 1},
-                {34, 98, -9, 2, 1},
-                {2, 3, 6, 9, -9}};
+        int[][] output1 = {{1, 4},
+                           {3, 2}};
 
-        return new Object[][] { {input, 2, 3, input23}, { input, 1,4, input14 } };
+        int[][] output2 = {{ 0,  98, -9, 5, 2,  4},
+                           { 6,   6, -6, 6, 6, -6},
+                           {-9, -10,  9, 1, 5, -8}};
+
+        return new Object[][]{{input1, output1}, {input2, output2}};
+    }
+
+    @Test(expectedExceptions = AssertionError.class)
+    public void negativeMatrixTest(){
+        new Variant12().matrixTask(new int[][] {{7},
+                                                {7, 7},
+                                                {7, 7, 7}});
     }
 }
+
